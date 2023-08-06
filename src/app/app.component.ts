bb02e4eaf1c21b17a446f4132229da15f7201641
TheGuyTheClasher <nishikant.bawache.nb@gmail.com>
2023-08-06 18:29:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calculatorApp';
+  equation = '0';
+  isFloat = false;
+  isInit = true;
+  isOff = false;
+
+  concatOperator(operator: string) {
+    if(this.isOff) return
+    if (operator === 'AC') {
+      this.equation = '0';
+      this.isInit = true;
+    } else {
+      if (!this.equation[this.equation.length - 1].match(/[-+*/]/)) {
+        this.equation += operator;
+        this.isFloat = false;
+      }
+    }
+  }
+
+  command(command: string) {
+    if (command === 'Off') {
+      if (!this.isOff) {
+        this.isOff = true;
+      } else {
+        this.isOff = false;
+      }
+    } else if (command === '%') {
+      if(this.isOff) return
+      if (!this.equation[this.equation.length - 1].match('%')) {
+        this.equation += '%';
+      }
+    } else if (command === 'DEL') {
+      if(this.isOff) return
+      if (this.equation.length === 1) {
+        this.equation = '0';
+        this.isInit = true;
+      } else {
+        this.equation = this.equation.substring(0, this.equation.length - 1);
+      }
+    }
+  }
+
+  addDecimal() {
+    if(this.isOff) return
+    this.isFloat = true;
+    this.equation += '.';
+  }
+
+  updateCurrNum(num: string) {
+    if(this.isOff) return
+    if (this.isInit) {
+      this.equation = num;
+      this.isInit = false;
+    } else {
+      this.equation += num;
+    }
+  }
+
+  calculate() {
+    if(this.isOff) return
+    try {
+      this.equation = eval(this.equation).toString();
+    } catch (error) {
+      this.equation = 'Error';
+    }
+  }
+
 }
